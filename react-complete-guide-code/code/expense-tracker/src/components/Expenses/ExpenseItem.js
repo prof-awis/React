4d17c8cb81c181d30645b5de1754d4de1692c988
prof-awis <awinoemanuel2000@gmail.com>
2023-04-05 23:47:12 +0300

    About useState

--- react-complete-guide-code/code/expense-tracker/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide-code/code/expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,22 @@
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  // let title = props.title;
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Kshs. {props.amount}</div>
+        <button onClick={clickHandler}>Change Item</button>
       </div>
     </Card>
   );
